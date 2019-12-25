e5a63b98c8db77fa1845ff6af45f7e1643d3dd6f
Jeff Lai <winfield301@gmail.com>
2019-12-25 14:07:18 +0800

    Set up slate editor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from "react";
+
+// Import the Slate editor factory.
+import { createEditor } from "slate";
+
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from "slate-react";
+
+import "./App.css";
 
 function App() {
+  // Create a Slate editor object that won't change across renders.
+  const editor = useMemo(() => withReact(createEditor()), []);
+
+  // Add the initial value when setting up our state.
+  const [value, setValue] = useState([
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }]
+    }
+  ]);
+
+  // Render the Slate context.
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+        <Editable />
+      </Slate>
     </div>
   );
 }
